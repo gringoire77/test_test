6bc0ef5f10de09dc4ccda98ec5171c62b1fef27f
gringoire77 <snacker@yandex.ru>
2020-05-05 20:16:22 +0000

    task 5.1 done

---
+++ docs/source/exercises/task_5_1_c.py
@@ -0,0 +1,30 @@
+london_co = {
+        'r1': {
+                    'location': '21 New Globe Walk',
+                    'vendor': 'Cisco',
+                    'model': '4451',
+                    'ios': '15.4',
+                    'ip': '10.255.0.1'
+                },
+        'r2': {
+                    'location': '21 New Globe Walk',
+                    'vendor': 'Cisco',
+                    'model': '4451',
+                    'ios': '15.4',
+                    'ip': '10.255.0.2'
+                },
+        'sw1': {
+                    'location': '21 New Globe Walk',
+                    'vendor': 'Cisco',
+                    'model': '3850',
+                    'ios': '3.6.XE',
+                    'ip': '10.255.0.101',
+                    'vlans': '10,20,30',
+                    'routing': True
+                }
+}
+
+name = input ('Введите имя устройства: ')
+keys = list(london_co[name].keys())
+param = input ('Введите имя параметра (' + ' '.join(keys) + '): ') 
+print((london_co.get(name)).get(param, 'Такого параметра нет'))
